13b83c715cdef81962af13277e8a9a8812c74317
atma0004 <atma0004@edu.eal.dk>
2017-02-02 14:01:42 +0100

    Add project files.

---
+++ modulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modulo
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("please enter a number!");
+            int number;
+            number=Convert.ToInt32(Console.ReadLine());
+            if(number%2==0)
+                {
+                Console.WriteLine("your number is even");
+                }
+            else
+                {
+                Console.WriteLine("your number is odd");
+                }
+            }
+        }
+    }
